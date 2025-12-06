09eedeab85743f205f25c1d605bbe12519fd0622
Salinger2333 <1565664814@qq.com>
2025-12-06 15:10:30 +0800

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,20 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
   },
   devServer: {
     static: "./dist",
